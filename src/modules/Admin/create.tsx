1c8215cb32d0e0597fae3d27c4e17df3e5b99bd4
ugwuonyiugwu <assistfuto@gmail.com>
2026-07-31 19:49:42 +0100

    update

--- src/modules/Admin/create.tsx
+++ src/modules/Admin/create.tsx
@@ -90,7 +90,7 @@ export function NewMonthWishClient({
       </div>
       <div className={`absolute inset-y-0 right-0 w-1/2 bg-[#2d3748] z-30 transition-transform duration-1500 ease-in-out ${isOpen ? 'translate-x-full' : 'translate-x-0'} flex items-center justify-start border-l border-amber-400/30 shadow-2xl`}>
         <div className="absolute left-3 text-amber-300 animate-pulse font-serif italic text-xs tracking-widest uppercase opacity-70">
-          July 🌸
+          August 🌸
         </div>
       </div>
 
